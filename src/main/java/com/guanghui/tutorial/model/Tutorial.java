8a1a880bb52d711c2942201828d929638fe1e220
Guanghui Wang <guanghui-wang@foxmail.com>
2022-04-28 18:38:03 +0800

    refactor: enhance entity

--- src/main/java/com/guanghui/tutorial/model/Tutorial.java
+++ src/main/java/com/guanghui/tutorial/model/Tutorial.java
@@ -2,25 +2,31 @@ package com.guanghui.tutorial.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity(name = "tutorials")
+@Entity()
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//Type definition error: [simple type, class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor and no properties discovered to create BeanSerializer (to avoid exception, disable SerializationFeature.FAIL_ON_EMPTY_BEANS) (through reference chain: com.guanghui.tutorial.model.Tutorial$HibernateProxy$sBbJgoiK[\"hibernateLaz
+@Builder
+//  Type definition error: [simple type, class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor and no properties discovered to create BeanSerializer (to avoid exception, disable SerializationFeature.FAIL_ON_EMPTY_BEANS) (through reference chain: com.guanghui.tutorial.model.Tutorial$HibernateProxy$sBbJgoiK[\"hibernateLaz
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})  // binary object issue
+@Table(name = "tutorials", uniqueConstraints = @UniqueConstraint(name = "title_unique", columnNames = "title"))
 public class Tutorial {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "tutorials_sequence",
+            sequenceName = "tutorials_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorials_sequence")
     private Long id;
 
+    @Column(name = "title", nullable = false)
     private String title;
     private String description;
     private Boolean published;
